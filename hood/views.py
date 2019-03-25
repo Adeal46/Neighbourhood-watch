e6d5fee79651632816a9ee8cfb6dff1eaea72f2f
Adeal <adililema3@gmail.com>
2019-03-25 12:24:14 +0300

    removed the hood.email for the high priority

--- hood/views.py
+++ hood/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from .models import neighbourhood,healthservices,Business,Health,Authorities,BlogPost,Profile,notifications,Comment
-from .email import send_priority_email
 from .forms import notificationsForm,ProfileForm,BlogPostForm,BusinessForm,CommentForm
 from decouple import config,Csv
 import datetime as dt
